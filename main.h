431fdf7fecc07109cb0359e22661b4e4ac5ad815
AmalNadifi <amal.nadifi9@gmail.com>
2023-07-27 13:10:35 -0700

    main header file and first operations handler added

---
+++ main.h
@@ -0,0 +1,81 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+/**
+ * struct CommandNode - This structure represents a node in a singly linked
+ * list for storing parsed script commands, including mode, line number,
+ * command, and argument.
+ * @line_number: line number of the command in the script.
+ * @cmd: Array containing the command and its argument.
+ * @Command_Mode: the command mode to be chosen by the user.
+ * @next: Pointer to the next node in the linked list.
+ * Return: ThE address of linked list.
+ */
+
+typedef struct CommandNode
+{
+	char *cmd[2];
+unsigned int line_number;
+unsigned int  Command_Mode;
+	struct CommandNode *next;
+} Commands;
+extern Commands *head;
+Commands *add_cmd(Commands **head, char *s, int i);
+void f_push(stack_t **stack, unsigned int line_number);
+void f_pall(stack_t **stack, unsigned int line_number);
+void f_pint(stack_t **stack, unsigned int line_number);
+void f_pop(stack_t **stack, unsigned int line_number);
+void f_swap(stack_t **stack, unsigned int line_number);
+void f_add(stack_t **stack, unsigned int line_number);
+void f_nop(stack_t **stack, unsigned int line_number);
+void handle_cmds(stack_t **stack);
+void free_linkedlist(stack_t **stack);
+void f_sub(stack_t **stack, unsigned int line_number);
+void f_div(stack_t **stack, unsigned int line_number);
+void f_mul(stack_t **stack, unsigned int line_number);
+void f_mod(stack_t **stack, unsigned int line_number);
+void f_pchar(stack_t **stack, unsigned int line_number);
+void f_pstr(stack_t **stack, unsigned int line_number);
+void f_rotl(stack_t **stack, unsigned int line_number);
+void f_rotr(stack_t **stack, unsigned int line_number);
+void f_stack(stack_t **stack, unsigned int line_number);
+void f_queue(stack_t **stack, unsigned int line_number);
+void f_pushq(stack_t **stack, unsigned int line_number);
+int is_comment_line(char **s);
+
+#endif /*MONTY_H*/
+
---
+++ op_handler.c
@@ -0,0 +1,116 @@
+#include "main.h"
+
+/**
+ * f_push - the function pushes agiven element to the Stack
+ * @stack: Pointer to the stack's double-linkedlist
+ * @line_number: CurrEnt line number.
+ */
+
+void f_push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *element = NULL;
+	stack_t *tempNode = *stack;
+
+	/* Checking if the stack is in the queue mode*/
+	if (head->Command_Mode)
+	{
+		f_pushq(stack, line_number);
+		return;
+	}
+	element = malloc(sizeof(stack_t));
+	if (!element)
+	{
+		dprintf(STDERR_FILENO, "Error: malloc failed\n");
+		free_linkedlist(stack);
+		exit(EXIT_FAILURE);
+	}
+	if ((head->cmd[1]))
+	{
+		dprintf(STDERR_FILENO, "L%d: usage: push integer\n", line_number);
+		free_linkedlist(stack);
+		free(element);
+		exit(EXIT_FAILURE);
+	}
+	element->n = atoi(head->cmd[1]);
+	element->next = NULL;
+	/* if stack is empty make the new element the top of it*/
+	if ((!stack))
+	{
+		element->prev = *stack;
+		*stack = element;
+		return;
+	}
+	/* if the stack is not empty traverse the stack to find the lastone */
+	while (tempNode->next)
+	{
+		tempNode = tempNode->next;
+	}
+	tempNode->next = element;
+	element->prev = tempNode;
+}
+ /**
+  * f_pall - the function prints the stacks values from thetop.
+  * @stack: the pointer to the stakcs double-linked list.
+  * @line_number: the CurrenT line number.
+  */
+
+void f_pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *ptr = *stack; /* to traverse the stack*/
+	(void)line_number;
+
+	if (ptr == NULL)
+		return; /* if stack is empty there is nothing to print*/
+	for (; ptr->next != NULL; ptr = ptr->next)
+		;
+	for (; ptr != NULL; ptr = ptr->prev)
+	{
+		printf("%d\n", ptr->n);
+	}
+}
+
+ /**
+  * free_linkedlist - the function frees all nodes of
+  * a linked list and associated data
+  * @stack: the POInter to the stack's double-linkedlist.
+  */
+
+void free_linkedlist(stack_t **stack)
+{
+	stack_t *node = NULL;
+	Commands *cmd = NULL;
+
+	while (*stack != NULL)
+	{
+		node = *stack;
+		*stack = (*stack)->next;
+		free(node);
+	}
+	while (head != NULL)
+	{
+		cmd = head;
+		head = head->next;
+		free(cmd->cmd[1]);
+		free(cmd->cmd[0]);
+		free(cmd);
+	}
+}
+/**
+ * f_pint - the function adds at the end of a list
+ * @stack: Pointer to the stack's double-linked list.
+ * @line_number: The line numBer.
+ */
+void f_pint(stack_t **stack, unsigned int line_number)
+{
+	stack_t *var = *stack;
+
+	if (!var)
+	{
+		dprintf(STDERR_FILENO, "L%d: can't pint, stack empty\n", line_number);
+		free_linked_list(stack);
+		exit(EXIT_FAILURE);
+	}
+	for (; var->next; var = var->next)
+		;
+	printf("%d\n", var->n);
+}
